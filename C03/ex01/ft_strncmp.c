1e9a4c00718f002e2b45d8b967d3fcbd1d6805ae
GMDPG98 <guimarsondpgarcia@gmail.com>
2022-05-18 16:53:05 -0300

    Function name correction

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -10,13 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	count;
 
 	count = 0;
 	while ((s1[count] != '\0') && (s2[count] != '\0')
-		&& (count < n) && (count < n))
+		&& (count < n))
 	{
 		if (s1[count] != s2[count])
 		{
